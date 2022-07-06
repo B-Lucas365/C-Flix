351af317efa191f5813001cb1ec41d8d15ac0a3d
B-Lucas365 <lucasrenan365@outlook.com>
2022-07-06 20:20:59 -0300

    swiper

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import {useState} from 'react'
+import {useState, useRef, useEffect} from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import "swiper/css/pagination";
+import "./styles.css";
+import { Pagination } from "swiper";
+
 import './App.scss'
 import {Movie} from './types/Movie'
 import {Card} from './Card'
 import {Loading} from './Loading'
+import {Header} from './Header'
 
 
 export const App = () => {
   const [movies, setMovies] = useState<Movie[]>([])
   const [loading, setLoading] = useState(false)
+  // const largura = useRef<any>(null)
+  // const [teste, setTeste] = useState(1)
+
+  // useEffect(()=> {
+  //   let width = largura.current.offsetWidth;
+
+  //   if(width < 480){
+  //     setTeste(1)
+  //   }else if(width > 481 && width < 768){
+  //     setTeste(2)
+  //   }else if(width > 769){
+  //     setTeste(4)
+  //   }
+    
+  // }, [largura])
 
   const loadMovies = async() => {
     setLoading(true)
@@ -22,17 +45,30 @@ export const App = () => {
   }
 
   return (
-    <div className='container'>
+    
+    <div className='container' >
+        <Header />
         <button className='button' onClick={loadMovies}>Carregar filmes</button>
 
         {loading && 
           <Loading />
         }
 
-        <div className='cardbox'>
+        <div className='cardbox' >
+        <Swiper
+          pagination={{
+            dynamicBullets: true,
+          }}
+          slidesPerView={3}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
           {movies.map((item, index) => (
-            <Card title={item.titulo} avatar={item.avatar} key={index}/>
+            <SwiperSlide key={index}>
+              <Card title={item.titulo} avatar={item.avatar} key={index}/>
+            </SwiperSlide>
           ))}
+        </Swiper>
         </div>
     </div>
   )
---
+++ src/Header/index.tsx
@@ -0,0 +1,10 @@
+import logo from '../assets/logo.svg'
+import './style.scss'
+
+export const Header = () => {
+    return (
+        <header>
+            <img src={logo} alt="" />
+        </header>
+    )
+}
